df2670fa0924dfa36dd055331a58bd0980cedb55
sergioleno <sergioleno@gmail.com>
2020-02-02 20:36:23 -0300

    fastfeet 8 criptografia de senha

--- backend/src/app/models/User.js
+++ backend/src/app/models/User.js
@@ -1,4 +1,5 @@
 import Sequelize, { Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -8,10 +9,17 @@ class User extends Model {
         //name, email, password_hash
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL, // campode "cache"
         password_hash: Sequelize.STRING,
       },
       { sequelize } // pode ter um segundo parâmetro com outras opções...ctrl espaço para acessar
     );
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8); // 8 niveis de criptografia
+      }
+    }); // execução automática baseada em ações do model
+    return this; // retorna o model inicializado
   }
 }
 
